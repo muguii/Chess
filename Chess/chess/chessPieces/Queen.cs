fca5d7e1f174d7a1c6e713d5c81a727fd8c6a780
Muriel Aurelio <murielaaurelio@gmail.com>
2020-04-20 17:04:37 -0300

    Queen

---
+++ Chess/chess/chessPieces/Queen.cs
@@ -0,0 +1,132 @@
+﻿using boardgame;
+
+namespace chess.chessPieces
+{
+    class Queen : ChessPiece
+    {
+        public Queen(Board board, Color color) : base(board, color)
+        {
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+            Position aux = new Position(0, 0);
+
+
+            //Above
+            aux.SetValues(Position.Row - 1, Position.Column);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Row--;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Northeast
+            aux.SetValues(Position.Row - 1, Position.Column + 1);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Row--;
+                aux.Column++;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Right
+            aux.SetValues(Position.Row, Position.Column + 1);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Column++;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Southeast
+            aux.SetValues(Position.Row + 1, Position.Column + 1);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Row++;
+                aux.Column++;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Below
+            aux.SetValues(Position.Row + 1, Position.Column);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Row++;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Southwest
+            aux.SetValues(Position.Row + 1, Position.Column - 1);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Row++;
+                aux.Column--;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Left
+            aux.SetValues(Position.Row, Position.Column - 1);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Column--;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+
+            //Northwest
+            aux.SetValues(Position.Row - 1, Position.Column - 1);
+            while (Board.PositionExists(aux) && !Board.ThereIsAPiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+                aux.Row--;
+                aux.Column--;
+            }
+            if (Board.PositionExists(aux) && IsThereOpponentePiece(aux))
+            {
+                mat[aux.Row, aux.Column] = true;
+            }
+
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "Q";
+        }
+    }
+}
